7d0cbb2437e94a9b5fea3bd372cda271fd923bad
Cristian Fernando Santos Ribero <csantosr@unal.edu.co>
2020-11-03 09:50:47 -0500

    Prettier changes

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,78 +1,96 @@
-import Storage from './storage'
+import Storage from "./storage";
 
 class Http {
-    constructor() {
-        this.APIUrl = 'http://localhost:8000'
-        this.storage = new Storage()
+  constructor() {
+    this.APIUrl = "http://localhost:8000";
+    this.storage = new Storage();
 
-        if (typeof Http.instance === 'object') {
-            return Http.instance
-        }
-
-        Http.instance = this
-        return this
+    if (typeof Http.instance === "object") {
+      return Http.instance;
     }
 
-    async manageFetch(url, method, body = {}, headers = {}) {
-        try {
-            const requestData = {
-                method,
-                headers: {
-                    ...headers,
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-            }
-            if (method !== 'GET') {
-                requestData.body = JSON.stringify(body)
-            }
-            let request = await fetch(url, requestData)
-            let json = await request.json()
-            return {
-                data: json,
-                status: request.status
-            }
-        } catch (error) {
-            console.error(`Http ${method} method on ${url} failed with error ${error}`)
-            throw Error(error)
-        }
-    }
+    Http.instance = this;
+    return this;
+  }
 
-    authHeader() {
-        return { 'Authorization': `Token ${localStorage.getItem('access_token')}` }
+  async manageFetch(url, method, body = {}, headers = {}) {
+    try {
+      const requestData = {
+        method,
+        headers: {
+          ...headers,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      };
+      if (method !== "GET") {
+        requestData.body = JSON.stringify(body);
+      }
+      let request = await fetch(url, requestData);
+      let json = await request.json();
+      return {
+        data: json,
+        status: request.status,
+      };
+    } catch (error) {
+      console.error(
+        `Http ${method} method on ${url} failed with error ${error}`
+      );
+      throw Error(error);
     }
+  }
 
-    async get(endpoint, headers = {}) {
-        return await this.manageFetch(this.APIUrl + endpoint, 'GET', {}, headers)
-    }
-    async post(endpoint, body, headers = {}) {
-        return await this.manageFetch(this.APIUrl + endpoint, 'POST', body, headers)
-    }
-    async put(endpoint, body, headers = {}) {
-        return await this.manageFetch(this.APIUrl + endpoint, 'PUT', body, headers)
-    }
-    async patch(endpoint, body, headers = {}) {
-        return await this.manageFetch(this.APIUrl + endpoint, 'PATCH', body, headers)
-    }
-    async delete(endpoint, headers = {}) {
-        return await this.manageFetch(this.APIUrl + endpoint, 'DELETE', {}, headers)
-    }
+  authHeader() {
+    return { Authorization: `Token ${localStorage.getItem("access_token")}` };
+  }
 
-    async authGet(endpoint) {
-        return await this.get(endpoint, this.authHeader())
-    }
-    async authPost(endpoint, body) {
-        return await this.post(endpoint, body, this.authHeader())
-    }
-    async authPut(endpoint, body) {
-        return await this.put(endpoint, body, this.authHeader())
-    }
-    async authPatch(endpoint, body) {
-        return await this.patch(endpoint, body, this.authHeader())
-    }
-    async authDelete(endpoint) {
-        return await this.delete(endpoint, this.authHeader())
-    }
+  async get(endpoint, headers = {}) {
+    return await this.manageFetch(this.APIUrl + endpoint, "GET", {}, headers);
+  }
+  async post(endpoint, body, headers = {}) {
+    return await this.manageFetch(
+      this.APIUrl + endpoint,
+      "POST",
+      body,
+      headers
+    );
+  }
+  async put(endpoint, body, headers = {}) {
+    return await this.manageFetch(this.APIUrl + endpoint, "PUT", body, headers);
+  }
+  async patch(endpoint, body, headers = {}) {
+    return await this.manageFetch(
+      this.APIUrl + endpoint,
+      "PATCH",
+      body,
+      headers
+    );
+  }
+  async delete(endpoint, headers = {}) {
+    return await this.manageFetch(
+      this.APIUrl + endpoint,
+      "DELETE",
+      {},
+      headers
+    );
+  }
+
+  async authGet(endpoint) {
+    return await this.get(endpoint, this.authHeader());
+  }
+  async authPost(endpoint, body) {
+    return await this.post(endpoint, body, this.authHeader());
+  }
+  async authPut(endpoint, body) {
+    return await this.put(endpoint, body, this.authHeader());
+  }
+  async authPatch(endpoint, body) {
+    return await this.patch(endpoint, body, this.authHeader());
+  }
+  async authDelete(endpoint) {
+    return await this.delete(endpoint, this.authHeader());
+  }
 }
 
 export default Http;
+
