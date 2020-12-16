9ecb186d5b1c86b32589e8956e0b3efee1a4b9d2
Cristian Fernando Santos Ribero <csantosr@unal.edu.co>
2020-12-16 16:17:26 -0500

    Showing difficulties on detail

--- src/views/signedIn/exercises/view_exercise.vue
+++ src/views/signedIn/exercises/view_exercise.vue
@@ -11,20 +11,32 @@
         <p class="light-font dark-text">{{ exercise.status }}</p>
         <h3 class="regular-font dark-text">Video subido:</h3>
         <video :src=" `http://localhost/media/${ exercise.video }` " controls></video>
-        <!-- <div class="patients-table">
-          <span class="regular-font">Paciente</span>
-          <span class="regular-font">Terapeuta</span>
-          <span class="regular-font">Fecha</span>
-          <template v-for="patient in scheduled_routines_patients" :key="patient.id">
-            <span class="light-font">{{
-              patient.patient.user.first_name + ' ' + patient.patient.user.last_name
-            }}</span>
-            <span class="light-font">{{
-              patient.therapist.user.first_name + ' ' + patient.therapist.user.last_name
-            }}</span>
-            <span class="light-font">{{ parseDate(new Date(patient.start_time)) }}</span>
+        <hr>
+        <div class="patients-table">
+          <span class="regular-font">Punto</span>
+          <span class="regular-font">Ángulo mínimo</span>
+          <span class="regular-font">Ángulo máximo</span>
+          <template v-for="point in tracked_points" :key="point.id">
+            <span class="light-font">{{point.verbose}}</span>
+            <span class="light-font">{{point.min_angle}}</span>
+            <span class="light-font">{{point.max_angle}}</span>
           </template>
-        </div> -->
+        </div> 
+        <hr>
+        <div class="diff-table">
+          <span class="regular-font">Nombre</span>
+          <span class="regular-font">Punto seguido</span>
+          <span class="regular-font">Ángulo mínimo</span>
+          <span class="regular-font">Ángulo máximo</span>
+          <template v-for="diff in difficulties" :key="diff.name">
+            <template v-for="(range, index) in diff.ranges" :key="index">
+              <span class="light-font">{{index === 0 ? diff.name : ''}}</span>
+              <span class="light-font">{{range.point_tracked.skeleton_point.verbose}}</span>
+              <span class="light-font">{{range.min_angle}}</span>
+              <span class="light-font">{{range.max_angle}}</span>
+            </template>
+          </template>
+        </div> 
       </div>
     </div>
     <div class="actions-container">
@@ -32,6 +44,10 @@
         <button class="btn btn-dark" v-on:click="() => $router.push({name: 'edit_exercise', params: {exercise_id: exercise.id}})" >
           <VideoAccount class="action-icon" />
           <span>Asignar video al ejercicio</span>
+        </button>
+        <button class="btn btn-dark" v-on:click="() => $router.push({name: 'edit_exercise', params: {exercise_id: exercise.id}})" >
+          <RunFast class="action-icon" />
+          <span>Asignar nueva dificultad al ejercicio</span>
         </button>
       </div>
     </div>
@@ -41,7 +57,7 @@
 <script>
 import Http from '@/lib/http';
 import '@/styles/views/view_routine.scss';
-import { VideoAccount } from "mdue";
+import { VideoAccount, RunFast } from "mdue";
 
 export default {
   name: 'ViewExercise',
@@ -50,6 +66,7 @@ export default {
   },
   components: {
     VideoAccount,
+    RunFast
   },
   methods: {
     async getExercise() {
@@ -66,15 +83,40 @@ export default {
         this.status = this.exercise.status;
         if (this.status === "Video procesado") {
           await this.getPointsTracked();
+          await this.getDifficulties();
         }
       } else {
         console.log("TODO: route to 404");
       }
     },
+    async getPointsTracked() {
+      const http = new Http();
+      const response = await http.authGet(
+        `/exercises/${this.exercise.id}/points_tracked/`
+      );
+      if (response.status !== 200) {
+        console.error("Error on fetch");
+        return;
+      }
+      this.tracked_points = response.data;
+    },
+    async getDifficulties() {
+      const http = new Http();
+      const response = await http.authGet(
+        `/exercises/${this.exercise.id}/difficulties/`
+      );
+      if (response.status !== 200) {
+        console.error("Error on fetch");
+        return;
+      }
+      this.difficulties = response.data;
+    },
   },
   data() {
     return {
       exercise: {},
+      tracked_points: [],
+      difficulties: []
     };
   },
 };
