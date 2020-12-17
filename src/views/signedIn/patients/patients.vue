8033c8508a2ac5934c51b5959d417dc81c0cac9d
Cristian Fernando Santos Ribero <csantosr@unal.edu.co>
2020-12-17 17:04:54 -0500

    Patients table view

---
+++ src/views/signedIn/patients/patients.vue
@@ -0,0 +1,56 @@
+<template>
+  <ListModel
+    title="Pacientes"
+    :table_info="{
+      columns: [
+        { query: 'id', verbose: 'ID' },
+        { query: 'patient.user.username', verbose: 'Paciente' },
+        { query: 'therapist.user.username', verbose: 'Terapeuta' },
+        { query: 'therapy.name', verbose: 'Terapia' },
+      ],
+      actions: ['detail'],
+      api_endpoint: `/therapist_patient?therapist_id=${therapist_id}`,
+      sorting_column: 'id'
+    }"
+    :has_new="false"
+    @go-to-create="$router.push({ name: 'new_therapy' })"
+    @go-to-update="goToUpdate"
+    @go-to-delete="goToDelete"
+    @go-to-detail="goToDetail"
+    v-if="therapist_id"
+  />
+</template>
+
+<script>
+import ListModel from '@/components/listModel';
+import Http from '@/lib/http';
+export default {
+  name: 'Patients',
+  components: {
+    ListModel
+  },
+  beforeMount() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    this.therapist_id = user.id;
+  },
+  methods: {
+    goToDelete(therapy_id) {
+      // const http = new Http();
+      // http.authDelete(`/therapies/${therapy_id}/`)
+      //   .then(response => this.$router.go())
+      //   .catch(response => console.error(response))
+    },
+    goToUpdate(therapy_id) {
+      this.$router.push({ name: 'edit_therapy', params: { therapy_id } });
+    },
+    goToDetail(therapy_id) {
+      this.$router.push({ name: 'view_therapy', params: { therapy_id } });
+    }
+  },
+  data() {
+    return {
+      therapist_id: null
+    }
+  }
+};
+</script>
