91ba46bf806e78e652188ddc155708342966814f
santivarelaagent-cmd <santivarela.agent@gmail.com>
2025-08-01 21:44:59 -0500

    first commit

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,16 +1,16 @@
-import Storage from "./storage";
+import Storage from './storage'
 
 class Http {
   constructor() {
-    this.APIUrl = "http://localhost:8000";
-    this.storage = new Storage();
+    this.APIUrl = 'http://172.27.80.1:8000'
+    this.storage = new Storage()
 
-    if (typeof Http.instance === "object") {
-      return Http.instance;
+    if (typeof Http.instance === 'object') {
+      return Http.instance
     }
 
-    Http.instance = this;
-    return this;
+    Http.instance = this
+    return this
   }
 
   async manageFetch(url, method, body = {}, headers = {}) {
@@ -19,32 +19,32 @@ class Http {
         method,
         headers: {
           ...headers,
-          "Content-Type": "application/json",
-          Accept: "application/json",
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
         },
-      };
-      if (method !== "GET") {
-        requestData.body = JSON.stringify(body);
       }
-      let request = await fetch(url, requestData);
-      if (method === "GET" || url.includes("login")) {
-        let json = await request.json();
+      if (method !== 'GET') {
+        requestData.body = JSON.stringify(body)
+      }
+      let request = await fetch(url, requestData)
+      if (method === 'GET' || url.includes('login')) {
+        let json = await request.json()
         return {
           data: json,
           status: request.status,
-        };
+        }
       } else {
         return {
           request,
-        };
+        }
       }
     } catch (error) {
       console.error(
         `Http ${method} method on ${url} failed with error ${error}`
-      );
-      console.error(error);
+      )
+      console.error(error)
       return {
-        error: true
+        error: true,
       }
     }
   }
@@ -55,82 +55,72 @@ class Http {
         method,
         headers,
         body: form,
-      };
-      let request = await fetch(url, requestData);
-      let json = await request.json();
+      }
+      let request = await fetch(url, requestData)
+      let json = await request.json()
       return {
         status: request.status,
         data: json,
-      };
+      }
     } catch (error) {
       console.error(
         `Http ${method} method on ${url} failed with error ${error}`
-      );
-      throw Error(error);
+      )
+      throw Error(error)
     }
   }
 
   authHeader() {
-    return { Authorization: `Token ${localStorage.getItem("access_token")}` };
+    return { Authorization: `Token ${localStorage.getItem('access_token')}` }
   }
 
   async get(endpoint, headers = {}) {
-    return await this.manageFetch(this.APIUrl + endpoint, "GET", {}, headers);
+    return await this.manageFetch(this.APIUrl + endpoint, 'GET', {}, headers)
   }
   async post(endpoint, body, headers = {}) {
-    return await this.manageFetch(
-      this.APIUrl + endpoint,
-      "POST",
-      body,
-      headers
-    );
+    return await this.manageFetch(this.APIUrl + endpoint, 'POST', body, headers)
   }
   async put(endpoint, body, headers = {}) {
-    return await this.manageFetch(this.APIUrl + endpoint, "PUT", body, headers);
+    return await this.manageFetch(this.APIUrl + endpoint, 'PUT', body, headers)
   }
   async patch(endpoint, body, headers = {}) {
     return await this.manageFetch(
       this.APIUrl + endpoint,
-      "PATCH",
+      'PATCH',
       body,
       headers
-    );
+    )
   }
   async delete(endpoint, headers = {}) {
-    return await this.manageFetch(
-      this.APIUrl + endpoint,
-      "DELETE",
-      {},
-      headers
-    );
+    return await this.manageFetch(this.APIUrl + endpoint, 'DELETE', {}, headers)
   }
   async postFormData(endpoint, form, headers = {}) {
     return await this.manageFetchFormData(
       this.APIUrl + endpoint,
-      "POST",
+      'POST',
       form,
       headers
-    );
+    )
   }
 
   async authGet(endpoint) {
-    return await this.get(endpoint, this.authHeader());
+    return await this.get(endpoint, this.authHeader())
   }
   async authPost(endpoint, body) {
-    return await this.post(endpoint, body, this.authHeader());
+    return await this.post(endpoint, body, this.authHeader())
   }
   async authPut(endpoint, body) {
-    return await this.put(endpoint, body, this.authHeader());
+    return await this.put(endpoint, body, this.authHeader())
   }
   async authPatch(endpoint, body) {
-    return await this.patch(endpoint, body, this.authHeader());
+    return await this.patch(endpoint, body, this.authHeader())
   }
   async authDelete(endpoint) {
-    return await this.delete(endpoint, this.authHeader());
+    return await this.delete(endpoint, this.authHeader())
   }
   async authPostFormData(endpoint, form) {
-    return await this.postFormData(endpoint, form, this.authHeader());
+    return await this.postFormData(endpoint, form, this.authHeader())
   }
 }
 
-export default Http;
+export default Http
